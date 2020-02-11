c3c17fb2261f825e0177f9c63292eda0eef8c86b
Aku <Anidimuka@gmail.com>
2020-02-11 02:59:33 +0100

    afegir esborrar acabat

--- JavaScript_Ajax/afegir_esborrar.js
+++ JavaScript_Ajax/afegir_esborrar.js
@@ -10,3 +10,22 @@ function borrar() {
   valores.length = 0;
   document.getElementById('resultado').innerHTML = valores + 'Cadena inicializada';
 }
+
+function reseteaInputArray() {
+  document.getElementById('valor').value = '';
+}
+
+var sumaDeValores = 0;
+function sumar() {
+  sumaDeValores = sumaDeValores + parseFloat(document.getElementById('numSuma').value);
+  document.getElementById('resultadoSuma').innerHTML = sumaDeValores;
+}
+
+function borrarSuma() {
+  sumaDeValores = 0;
+  document.getElementById('resultadoSuma').innerHTML = 'Suma inicializada';
+}
+
+function reseteaInputSuma() {
+  document.getElementById('numSuma').value = '';
+}
